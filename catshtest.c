94365f5286730d9a6382fe90b5b08c74710d3d26
MJVegad <mihir.vegad96@gmail.com>
2016-05-26 14:47:11 +0530

    Report outline added.

--- catshtest.c
+++ catshtest.c
@@ -7,7 +7,7 @@ int main()
 {
 	long ret;
 	printf("invoking systemcall.\n");
-	char str[]="R F 2:2:2:2:2:2 1.2.3.4";
+	char str[]="R L 00:22:4d:af:c8:f7 10.129.26.28 0 3.3.3.3";
 	ret = syscall(326, str);
 	if(ret<0)
 		exit(1);
